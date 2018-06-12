e74ed0153e45e2ff6a0e371886d38df90e21abdb
Drew Hawthorne <hawthord@oregonstate.edu>
2018-06-11 23:45:01 -0700

    Update UrlValidatorTest.java

--- projects/hawthord/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/hawthord/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -21,22 +21,70 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+//You can use this function to implement your manual testing  
+      String[] schemes = {"http","https"};
+      UrlValidator urlValidator = new UrlValidator(schemes);
+      if (urlValidator.isValid("www.a.com")) {
+          System.out.println("url is valid");
+      } else {
+          System.out.println("url is invalid");
+      }
+      System.out.println(urlValidator.isValid("The Rain"));
+   
+   
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+     //You can use this function to implement your First Partition testing
+      //done to only test parts of a URL, not an entire one
+         String[] schemes = {"http","https"};
+         UrlValidator urlValidator = new UrlValidator(schemes);
+         String[] urlPass = {"https://", "https://www.", "https://www.google.", "https://www.com/" };
+         for(String urls: urlPass)
+         if (urlValidator.isValid(urls)) {
+             System.out.println("url is valid");
+             } else {
+               System.out.println("url is invalid");
+              }
 
    }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
 
+ public void testYourSecondPartition(){
+         //You can use this function to implement your Second Partition testing     
+          //done to only test parts of a URL, not an entire one
+for(int p=0; p < 2; p++) {
+    if (p==0) {
+         String[] schemes = {"http","https"};
+         UrlValidator urlValidator = new UrlValidator(schemes);
+    }
+else{
+    UrlValidator urlValidator = new UrlValidator(null);
+}
+         String[] newSchemes = {"https://", "http", "https"};
+         String[] newAuthority = {"www.google.com","goo.gle.com", "google.com"};
+         String[] newPath = {"/search", "//test","",};
+         String[] newQuery = {"?q=best+way+to+use+java", "?/help"};
+         for(int i =0; i < newSchemes.length; i++) {
+             for(int j =0; j < newAuthority.length; j++) {
+                 for(int k =0; k < newPath.length; k++) {
+                     for(int l =0; l < newQuery.length; l++) {
+                         String Url2Check = new String(newSchemes[i]+newAuthority[j]+newPath[k]+newQuery[l]);
+                         System.out.println(Url2Check);
+                     if (urlValidator.isValid(Url2Check)) {
+                        System.out.println("valid");
+                         } else {
+                         System.out.println("invalid");
+                         } 
+                     }
+                 }     
+             }
+         }
+}
    }
-   //You need to create more test cases for your Partitions if you need to 
+
+
    
    public void testIsValid()
    {
